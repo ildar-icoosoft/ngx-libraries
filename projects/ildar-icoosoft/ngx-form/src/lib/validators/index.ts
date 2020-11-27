98ea234d2416f76d52f3ef94bdec9fa31be627c7
ildar <ildar.icoosoft@gmail.com>
2020-11-28 04:17:12 +0500

    interm

---
+++ projects/ildar-icoosoft/ngx-form/src/lib/validators/index.ts
@@ -0,0 +1,28 @@
+import { FormControl, FormGroup } from '@angular/forms';
+import { omit as _omit, isEmpty as _isEmpty } from 'lodash';
+
+export const validateEqual = (
+  a: FormControl | string,
+  b: FormControl | string
+) => {
+  return function(group: FormGroup): { [s: string]: boolean } {
+    const controlA = a instanceof FormControl ? a : group.root.get(a);
+    const controlB = b instanceof FormControl ? b : group.root.get(b);
+
+    const controlBErrors = controlB.errors;
+
+    if (controlA && controlB && controlA.value !== controlB.value) {
+      controlB.setErrors(
+        Object.assign({}, controlBErrors, {
+          mismatch: true
+        })
+      );
+    } else {
+      if (controlB.hasError('mismatch')) {
+        const newErrors = _omit(controlBErrors, 'mismatch');
+        controlB.setErrors(_isEmpty(newErrors) ? null : newErrors);
+      }
+    }
+    return null;
+  };
+};

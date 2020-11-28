af811471837487f2e619fb27974a908e5b5772d2
ildar-icoosoft <ildar.icoosoft@gmail.com>
2020-11-28 05:44:11 +0500

    fix: public API exports (#6)

--- projects/ildar-icoosoft/ngx-form/src/public-api.ts
+++ projects/ildar-icoosoft/ngx-form/src/public-api.ts
@@ -7,6 +7,7 @@ export * from './lib/ngx-form.module';
 
 export * from './lib/components/form-validation-errors/form-validation-errors.component';
 export * from './lib/components/validation-control/validation-control.component';
+export * from './lib/pipes/validation-message.pipe';
 
 export * from './lib/interfaces/form-error';
 export * from './lib/utils/error';

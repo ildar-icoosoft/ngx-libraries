d19c50a6d16c8737d5f1ad714d532fc38f5a4f78
ildar <ildar.icoosoft@gmail.com>
2020-11-28 05:24:36 +0500

    interm

--- projects/ildar-icoosoft/ngx-form/src/public-api.ts
+++ projects/ildar-icoosoft/ngx-form/src/public-api.ts
@@ -2,6 +2,12 @@
  * Public API Surface of ngx-form
  */
 
-export * from './lib/ngx-form.service';
-export * from './lib/ngx-form.component';
 export * from './lib/ngx-form.module';
+
+
+export * from './lib/components/form-validation-errors/form-validation-errors.component';
+export * from './lib/components/validation-control/validation-control.component';
+
+export * from './lib/interfaces/form-error';
+export * from './lib/utils/error';
+export * from './lib/validators/index';

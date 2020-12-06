0b29adb072fc00d1e33d91ae6c21d4f150ee81ce
ildar-icoosoft <ildar.icoosoft@gmail.com>
2020-12-06 14:56:16 +0500

    feat: NGXLIBRARIES-13 create unsubscribe method (#14)

---
+++ projects/ildar-icoosoft/ngx-common/src/lib/services/unsubscribe.service.ts
@@ -0,0 +1,16 @@
+import {Injectable, OnDestroy} from '@angular/core';
+import {Observable, Subject} from 'rxjs';
+
+@Injectable()
+export class UnsubscribeService extends Observable<void> implements OnDestroy {
+  readonly ngUnsubscribe$ = new Subject<void>();
+
+  constructor() {
+    super(subscriber => this.ngUnsubscribe$.subscribe(subscriber));
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe$.next();
+    this.ngUnsubscribe$.complete();
+  }
+}

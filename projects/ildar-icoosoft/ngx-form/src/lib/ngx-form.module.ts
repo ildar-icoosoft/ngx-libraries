372ae37a460e05f13313bf78658acc65d0bba5ef
ildar <ildar.icoosoft@gmail.com>
2020-12-06 08:21:54 +0500

    interm

--- projects/ildar-icoosoft/ngx-form/src/lib/ngx-form.module.ts
+++ projects/ildar-icoosoft/ngx-form/src/lib/ngx-form.module.ts
@@ -7,45 +7,9 @@ import {NgxFormModuleConfig} from './interfaces/ngx-form-module-config';
 import {NGX_FORM_MODULE_CONFIG} from './constants/ngx-form-module-config';
 import {DynamicFormComponent} from './components/dynamic-form/dynamic-form.component';
 import {DynamicFieldDirective} from './directives/dynamic-field.directive';
-import {FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-import {validateEqual} from './validators';
-import {DynamicFieldData} from './interfaces/dynamic-field-data';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {defaultNgxFormModuleConfig} from './configs/ngx-form-mdule.config';
 
-const defaultConfig: NgxFormModuleConfig = {
-  validators: {
-    required: {
-      isGroupValidator: false,
-      validator: () => {
-        return Validators.required;
-      }
-    },
-    email: {
-      isGroupValidator: false,
-      validator: () => {
-        return Validators.email;
-      }
-    },
-    passwordMatch: {
-      isGroupValidator: true,
-      validator: (fieldData: DynamicFieldData) => {
-        const a = 'password';
-        const b = fieldData.name;
-
-        return validateEqual(a, b);
-      }
-    }
-  },
-  errorMessages: {
-    required: 'This field is required',
-    email: 'Wrong email format',
-    mismatch: 'Field values mismatch',
-    minlength: 'Field length is too short must be {requiredLength}, actual is {actualLength}',
-    maxlength: 'Field length is too long must be {requiredLength}, actual is {actualLength}',
-    age: 'The age under {requiredAge} is not accepted',
-    min: 'The minimum value for an input field is {min}',
-    max: 'The maximum value for an input field is {max}',
-  }
-};
 
 @NgModule({
   declarations: [FormValidationErrorsComponent, ValidationControlComponent, ValidationMessagePipe, DynamicFormComponent, DynamicFieldDirective],
@@ -57,7 +21,7 @@ const defaultConfig: NgxFormModuleConfig = {
   exports: [FormValidationErrorsComponent, ValidationControlComponent, ValidationMessagePipe, DynamicFormComponent, DynamicFieldDirective],
   providers: [{
     provide: NGX_FORM_MODULE_CONFIG,
-    useValue: defaultConfig
+    useValue: defaultNgxFormModuleConfig
   }]
 })
 export class NgxFormModule {
@@ -67,9 +31,9 @@ export class NgxFormModule {
       providers: [{
         provide: NGX_FORM_MODULE_CONFIG,
         useValue: Object.assign({}, {
-          fields: Object.assign({}, defaultConfig.fields, config.fields),
-          validators: Object.assign({}, defaultConfig.validators, config.validators),
-          errorMessages: Object.assign({}, defaultConfig.errorMessages, config.errorMessages),
+          fields: Object.assign({}, defaultNgxFormModuleConfig.fields, config.fields),
+          validators: Object.assign({}, defaultNgxFormModuleConfig.validators, config.validators),
+          errorMessages: Object.assign({}, defaultNgxFormModuleConfig.errorMessages, config.errorMessages),
         })
       }]
     };

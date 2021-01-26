32b1cb982239b7494fc2b1ae27144900a2d95859
Ildar Farrakhov <ildar.icoosoft@gmail.com>
2021-01-26 21:29:49 +0500

    interm

--- projects/ildar-icoosoft/ngx-form/src/lib/ngx-form.module.ts
+++ projects/ildar-icoosoft/ngx-form/src/lib/ngx-form.module.ts
@@ -1,22 +1,18 @@
 import {ModuleWithProviders, NgModule} from '@angular/core';
-import {FormValidationErrorsComponent} from './components/form-validation-errors/form-validation-errors.component';
-import {ValidationControlComponent} from './components/validation-control/validation-control.component';
-import {ValidationMessagePipe} from './pipes/validation-message.pipe';
 import {CommonModule} from '@angular/common';
-import {NgxFormModuleConfig} from './interfaces/ngx-form-module-config';
 import {NGX_FORM_MODULE_CONFIG} from './constants/ngx-form-module-config';
-import {DynamicFormComponent} from './components/dynamic-form/dynamic-form.component';
-import {DynamicFieldDirective} from './directives/dynamic-field.directive';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {defaultNgxFormModuleConfig} from './configs/ngx-form-mdule.config';
-import { InputComponent } from './components/input/input.component';
-import { TextareaComponent } from './components/textarea/textarea.component';
-import { SelectComponent } from './components/select/select.component';
-import { HtmlComponent } from './components/html/html.component';
-import { FieldsetComponent } from './components/fieldset/fieldset.component';
-import { MultiFieldsetComponent } from './components/multi-fieldset/multi-fieldset.component';
-import { ReCaptchaComponent } from './components/re-captcha/re-captcha.component';
 import {RecaptchaFormsModule, RecaptchaModule} from 'ng-recaptcha';
+import {
+  DynamicFormComponent, FieldsetComponent,
+  FormValidationErrorsComponent, HtmlComponent,
+  InputComponent, MultiFieldsetComponent, ReCaptchaComponent, SelectComponent, TextareaComponent,
+  ValidationControlComponent
+} from "./components";
+import {ValidationMessagePipe} from "./pipes";
+import {DynamicFieldDirective} from "./directives";
+import {NgxFormModuleConfig} from "./interfaces";
 
 
 @NgModule({
@@ -52,7 +48,8 @@ export class NgxFormModule {
         provide: NGX_FORM_MODULE_CONFIG,
         useValue: Object.assign({}, {
           fields: Object.assign({}, defaultNgxFormModuleConfig.fields, config.fields),
-          validators: Object.assign({}, defaultNgxFormModuleConfig.validators, config.validators),
+          fieldValidators: Object.assign({}, defaultNgxFormModuleConfig.fieldValidators, config.fieldValidators),
+          groupValidators: Object.assign({}, defaultNgxFormModuleConfig.groupValidators, config.groupValidators),
           errorMessages: Object.assign({}, defaultNgxFormModuleConfig.errorMessages, config.errorMessages),
         })
       }]

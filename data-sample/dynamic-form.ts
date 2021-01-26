3a6425773131abe41e2af9259074f0142c59a517
Ildar Farrakhov <ildar.icoosoft@gmail.com>
2021-01-26 22:08:30 +0500

    interm

--- data-sample/dynamic-form.ts
+++ data-sample/dynamic-form.ts
@@ -26,15 +26,11 @@ export const dynamicFormData: DynamicForm = {
       label: 'Text <span class="required-sign">(*)</span>',
       name: 'text',
       type: 'text',
-      validators: [],
+      validators: [{"name": "required", "options": []}],
       options: [
         {
           name: 'placeholder',
           value: 'text placeholder',
-        },
-        {
-          name: 'validators',
-          value: '[{"name":"required","options":[]}]',
         },
         {
           name: 'htmlOptions',
@@ -47,15 +43,11 @@ export const dynamicFormData: DynamicForm = {
       label: 'Textarea <span class="required-sign">(*)</span>',
       name: 'textarea',
       type: 'textarea',
-      validators: [],
+      validators: [{"name": "required", "options": []}],
       options: [
         {
           name: 'placeholder',
           value: 'text plaeeholder',
-        },
-        {
-          name: 'validators',
-          value: '[{"name":"required","options":[]}]',
         },
         {
           name: 'htmlOptions',
@@ -68,15 +60,11 @@ export const dynamicFormData: DynamicForm = {
       label: 'Email <span class="required-sign">(*)</span>',
       name: 'email',
       type: 'email',
-      validators: [],
+      validators: [{"name": "required", "options": []}, {"name": "email", "options": []}],
       options: [
         {
           name: 'placeholder',
           value: 'email placeholder',
-        },
-        {
-          name: 'validators',
-          value: '[{"name":"required","options":[]}, {"name":"email","options":[]}]',
         },
         {
           name: 'htmlOptions',
@@ -89,15 +77,11 @@ export const dynamicFormData: DynamicForm = {
       label: 'Password <span class="required-sign">(*)</span>',
       name: 'password',
       type: 'password',
-      validators: [],
+      validators: [{"name": "required", "options": []}],
       options: [
         {
           name: 'placeholder',
           value: 'password placeholder',
-        },
-        {
-          name: 'validators',
-          value: '[{"name":"required","options":[]}]',
         },
         {
           name: 'htmlOptions',
@@ -110,15 +94,11 @@ export const dynamicFormData: DynamicForm = {
       label: 'Confirm Password <span class="required-sign">(*)</span>',
       name: 'confirmPassword',
       type: 'password',
-      validators: [],
+      validators: [{"name": "required", "options": []}],
       options: [
         {
           name: 'placeholder',
           value: 'confirm password placeholder',
-        },
-        {
-          name: 'validators',
-          value: '[{"name":"required","options":[]}]',
         },
         {
           name: 'htmlOptions',
@@ -138,9 +118,29 @@ export const dynamicFormData: DynamicForm = {
           value: 'twitter-share-button',
         },
         {
-          name: 'options',
+          name: 'selectOptions',
           value:
-            '[{"name":"twitter-share-button","label":"Share","position":"0"},{"name":"twitter-follow-button","label":"Follow","position":"1"},{"name":"twitter-hashtag-button","label":"Hashtag","position":"2"},{"name":"twitter-mention-button","label":"Mention","position":"3"},{"name":"twitter-tweet","label":"Tweet","position":"4"},{"name":"twitter-tweet","label":"Tweet","position":"4"},{"name":"twitter-timeline","label":"Timeline","position":"5"}]',
+            [
+              {"name": "twitter-share-button", "label": "Share"},
+              {
+                "name": "twitter-follow-button",
+                "label": "Follow"
+              },
+              {
+                "name": "twitter-hashtag-button",
+                "label": "Hashtag"
+              },
+              {"name": "twitter-mention-button", "label": "Mention"},
+              {
+                "name": "twitter-tweet",
+                "label": "Tweet"
+              },
+              {"name": "twitter-tweet", "label": "Tweet"},
+              {
+                "name": "twitter-timeline",
+                "label": "Timeline",
+              }
+            ],
         },
         {
           name: 'htmlOptions',
@@ -249,7 +249,7 @@ export const dynamicFormData: DynamicForm = {
       label: 'Recaptcha',
       name: 'recaptcha',
       type: 'reCaptcha',
-      validators: [],
+      validators: [{"name": "required", "options": []}],
       options: [
         {
           name: 'doNotEncrypt',
@@ -258,10 +258,6 @@ export const dynamicFormData: DynamicForm = {
         {
           name: 'doNotSave',
           value: '1',
-        },
-        {
-          name: 'validators',
-          value: '[{"name":"required","options":[]}]',
         },
         {
           name: 'htmlOptions',

346d55dac61ffee15d55dd5907b40773b8fde1c4
Ildar Farrakhov <ildar.icoosoft@gmail.com>
2021-04-25 19:44:27 +0500

    interm

--- projects/ildar-icoosoft/ngx-common/src/lib/pipes/array-includes.pipe.spec.ts
+++ projects/ildar-icoosoft/ngx-common/src/lib/pipes/array-includes.pipe.spec.ts
@@ -1,8 +1,8 @@
 import { ArrayIncludesPipe } from './array-includes.pipe';
 
-xdescribe('ArrayIncludesPipe', () => {
- /* it('create an instance', () => {
+describe('ArrayIncludesPipe', () => {
+  it('create an instance', () => {
     const pipe = new ArrayIncludesPipe();
     expect(pipe).toBeTruthy();
-  });*/
+  });
 });

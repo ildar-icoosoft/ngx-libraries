bbda1bc374094c7aa3fb359685db0d31b12bfcd5
ildar-icoosoft <ildar.icoosoft@gmail.com>
2021-07-01 13:43:01 +0500

    feat: NGXFORM-8 Добавить в интерфейс FieldComponentType методы getCssClass, isHidden, getFormGroupCssClass, getLabelCssClass, needToShowLabelOutside (#38)

--- projects/ildar-icoosoft/ngx-form/src/lib/types/field-component-type.ts
+++ projects/ildar-icoosoft/ngx-form/src/lib/types/field-component-type.ts
@@ -1,6 +1,17 @@
 import { Component } from '@angular/core';
 import { ControlValueAccessor } from '@angular/forms';
+import { DynamicField } from './dynamic-field';
 
 export interface FieldComponentType extends Component {
   getFormElement(): Component & ControlValueAccessor;
+
+  getCssClass(fieldData: DynamicField): string;
+
+  isHidden(fieldData: DynamicField): boolean;
+
+  getFormGroupCssClass(fieldData: DynamicField): string;
+
+  getLabelCssClass(fieldData: DynamicField): string;
+
+  needToShowLabelOutside(fieldData: DynamicField): boolean;
 }
